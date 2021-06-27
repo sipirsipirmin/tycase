5a4a935722a2c33eae0221fc1b7c55afe11aaf8a
Tunahan Dursun <tunahan.dursun@metglobal.com>
2021-06-27 16:55:15 +0300

    nginx config creation strategy changed

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from clusters import cluster_config_file_paths
 
 ANN_KEY = os.environ.get('ANN_KEY','hayde.trendyol.io/enabled')
 ANN_VALUE = os.environ.get('ANN_VALUE', 'true')
-NGINX_CONFIG_PATH = os.environ.get('NGINX_CONFIG_PATH','/etc/nginx/conf.d/')
+NGINX_CONFIG_PATH = os.environ.get('NGINX_CONFIG_PATH', '/etc/nginx/conf.d/')
 DOMAIN_NAME = os.environ.get("DOMAIN_NAME", 'sipirsipirmin.com')
 
 
@@ -17,9 +17,10 @@ handler.setLevel(logging.DEBUG)
 logger.addHandler(handler)
 
 class ServiceAnnotationWatcher:
-    def __init__(self, config_file):
+    def __init__(self, config_file, cluster_index):
         self.config_file = config_file
         self.kube_client = self.initialize_kube_client()
+        self.cluster_index = cluster_index
         all_services = self.get_services()
         self.compatible_services = self.get_annotation_compatible_services(all_services)
 
@@ -52,10 +53,9 @@ class ServiceAnnotationWatcher:
             
             if len(self.compatible_services.keys()) != len(tmp_compatible_services.keys()): # hımm!
                 self.compatible_services = tmp_compatible_services
-
-                logger.info("Event: %s %s %s" % (event['type'], event['object'].kind, event['object'].metadata.name))
-                logger.info("Something changed. Nginx configs will be update")
-                create_nginx_configuration_file_for_compatible_services(tmp_compatible_services)
+                service_name = event['object'].metadata.name
+                logger.info("Event: %s %s %s" % (event['type'], event['object'].kind, service_name))
+                create_nginx_configuration_file_for_compatible_services(tmp_compatible_services, self.cluster_index )
                 await asyncio.sleep(0)
 
 
@@ -63,22 +63,25 @@ def get_nginx_template():
     template_file = open("nginx.conf.template", "r")
     template = template_file.read()
     template_file.close()
+    
     return template
 
 
-def create_nginx_configuration_file_for_compatible_services(compatible_services):
+
+def create_nginx_configuration_file_for_compatible_services(compatible_services, cluster_index):
     nginx_template = get_nginx_template()
+    services_configs = []    
     for service_name in compatible_services.keys():
         nginx_server_name = '.'.join([service_name, DOMAIN_NAME])
-        nginx_conf_file_name = '.'.join([nginx_server_name, 'conf'])
-        nginx_conf_file_path = os.path.join(NGINX_CONFIG_PATH, nginx_conf_file_name)
-
-        nginx_config_file = open(nginx_conf_file_path, 'w')
-        nginx_config_file.write(nginx_template %(
+        services_configs.append(nginx_template %(
                                             nginx_server_name,compatible_services[service_name]['cluster_ip'],
                                             compatible_services[service_name]['port'])
                                             )
-        nginx_config_file.close()
+    cluster_config_file = '.'.join([str(cluster_index), 'conf'])
+    nginx_config_file = open(os.path.join(NGINX_CONFIG_PATH,cluster_config_file), 'w')
+    nginx_config_file.write('\n'.join(services_configs))
+    nginx_config_file.close()
+    
     os.system("systemctl reload nginx")  # well..
 
 
@@ -86,9 +89,8 @@ clusters = []
 
 if __name__ == "__main__":
     # initialize cluster watchers
+    ioloop = asyncio.get_event_loop()
     for index, cluster_config_file_path in enumerate(cluster_config_file_paths):
-        clusters.append(ServiceAnnotationWatcher(config_file=cluster_config_file_path))
-
-        ioloop = asyncio.get_event_loop()
+        clusters.append(ServiceAnnotationWatcher(config_file=cluster_config_file_path, cluster_index=index))
         ioloop.create_task(clusters[index].fallow_the_white_rabbit())
-        ioloop.run_forever()
+    ioloop.run_forever()
