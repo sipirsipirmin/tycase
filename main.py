334402151c683cefabe7b555747c0a4f3b9e73ef
Tunahan Dursun <tunahan.dursun@metglobal.com>
2021-06-27 14:30:27 +0300

    watch services

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-from kubernetes import client, config
-import os
+from kubernetes import client, config, watch
+import asyncio
+import logging
+import os, sys
 
 ANN_KEY = 'hayde.trendyol.io/enabled'
 ANN_VALUE = 'true'
 NGINX_CONFIG_PATH = '/etc/nginx/conf.d/'
 DOMAIN_NAME = 'sipirsipirmin.com'
 
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+logger.addHandler(handler)
 
 def initialize_kube_client():
     config.load_kube_config()
@@ -27,7 +36,7 @@ def get_annotation_compatible_services(services):
                 annotation_positive[i.metadata.name] = dict()
                 annotation_positive[i.metadata.name]['cluster_ip'] = i.spec.cluster_ip
                 annotation_positive[i.metadata.name]['port'] = i.spec.ports[0].port
-        return annotation_positive
+    return annotation_positive
 
 def get_nginx_template():
     template_file = open("nginx.conf.template", "r")
@@ -54,4 +63,20 @@ all_services = get_services(kube_client)
 compatible_services = get_annotation_compatible_services(all_services)
 nginx_template = get_nginx_template()
 
-create_nginx_configuration_file_for_compatible_services(compatible_services, nginx_template)
+#create_nginx_configuration_file_for_compatible_services(compatible_services, nginx_template)
+async def fallow_the_white_rabbit():
+    w = watch.Watch()
+    for event in w.stream(kube_client.list_service_for_all_namespaces, _request_timeout=120):
+        tmp_compatible_services = get_annotation_compatible_services(get_services(kube_client))
+        
+        if len(compatible_services.keys()) != len(tmp_compatible_services.keys()):
+            logger.info("Event: %s %s %s" % (event['type'], event['object'].kind, event['object'].metadata.name))
+            logger.info("Something changed. Nginx configs will be update")
+            create_nginx_configuration_file_for_compatible_services(tmp_compatible_services, nginx_template)
+            await asyncio.sleep(0) 
+
+ioloop = asyncio.get_event_loop()
+
+ioloop.create_task(fallow_the_white_rabbit())
+
+ioloop.run_forever()
